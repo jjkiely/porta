a29d1aed9583c55de4526c9c0bd110e61e665834
Nidhi Soni <nidhisoni4411@gmail.com>
2022-09-16 12:53:04 +0530

    updated worker

--- app/workers/message_worker.rb
+++ app/workers/message_worker.rb
@@ -8,8 +8,6 @@ class MessageWorker
   def perform(recipients, attributes)
     message = Message.new(attributes)
     message.sender_id = attributes["sender_id"]
-    message.body = attributes["body"]
-    message.subject = attributes["subject"]
 
     # ATTENTION:
     #
